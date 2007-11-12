69b14164d393222590694dcf3ecc1c1569f590a1
AABiryukov <AABiryukov@54da555d-b63e-0410-a441-ad8f353693c9>
2007-11-12 08:39:51 +0000

    fixed copyright

--- AssemblyInfo.cs
+++ AssemblyInfo.cs
@@ -1,14 +1,6 @@
 //-------------------------------------------------------------------------------------------------
-// <copyright file="AssemblyInfo.cs" company="Microsoft">
-//    Copyright (c) Microsoft Corporation.  All rights reserved.
-//    
-//    The use and distribution terms for this software are covered by the
-//    Common Public License 1.0 (http://opensource.org/licenses/cpl.php)
-//    which can be found in the file CPL.TXT at the root of this distribution.
-//    By using this software in any fashion, you are agreeing to be bound by
-//    the terms of this license.
-//    
-//    You must not remove this notice, or any other, from this software.
+// <copyright file="AssemblyInfo.cs" company="SITRONICS Telecom Solutions">
+//    GPL v3
 // </copyright>
 // 
 // <summary>
@@ -26,7 +18,7 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("SITRONICS Telecom Solutions")]
 //[assembly: AssemblyProduct("Windows Installer XML Toolset")]
-[assembly: AssemblyCopyright("Copyright © SITRONICS Telecom Solutions. All rights reserved.")]
+[assembly: AssemblyCopyright("GPL")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 [assembly: CLSCompliant(true)]
